f430f37aa052a0c0233f8b9a7cdf550998290c06
Max Bronstring <max.brons@hotmail.com>
2019-09-09 12:14:26 +0200

    Opdracht bijgewerkt

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -6,3 +6,27 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let circle = [];
+
+function animate(){
+    requestAnimationFrame(animate);   
+    let color = "rgb(" + getRandNumber(255) + "," + getRandNumber(255) + "," + getRandNumber(255) + ")";
+    let rvar = new Point(new Vector2d(getRandNumber(width),getRandNumber(height)),100,color);
+    circle.push(rvar);
+
+    for (let index = 0; index < circle.length; index++) {
+        circle[index].radius++;
+        circle[index].draw(context);
+
+        if(circle[index].radius > 50)
+            circle.splice(index,1);
+    }
+}
+
+animate();
+
+function getRandNumber(max){
+    let ans = Math.floor(Math.random() * max);
+    return ans;
+}
