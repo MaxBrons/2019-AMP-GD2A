dbda8dc58c97a3192bdd363551271c08593b6cb4
Max Bronstring <max.brons@hotmail.com>
2019-11-08 16:16:08 +0100

    Update my car

--- html/12myCar/script.js
+++ html/12myCar/script.js
@@ -17,6 +17,7 @@ car.velocity = new Vector2d(carVelocity,0);
 let frontwheel = new Image();
 frontwheel.src = "wheel.png";
 frontwheel.position = new Vector2d(0,0);
+frontwheel.rotation = 0;
 
 let rearwheel = new Image();
 rearwheel.src = "wheel.png";
@@ -34,11 +35,22 @@ function animate(){
   car.position.add(car.velocity)
   context.drawImage(car, car.position.dx, car.position.dy);
 
+  context.save();
   frontwheel.position.vectorSum(car.position, new Vector2d(132,120));
-  context.drawImage(frontwheel, frontwheel.position.dx, frontwheel.position.dy);
+  frontwheel.position.add(new Vector2d(80,75));
+  context.translate(frontwheel.position.dx,frontwheel.position.dy);
+  context.rotate(frontwheel.position.dx/(frontwheel.height/2));
+  context.drawImage(frontwheel, -frontwheel.width/2, -frontwheel.height/2);
+  context.restore();
+
+  context.save();
+  rearwheel.position.vectorSum(car.position, new Vector2d(132,120));
+  rearwheel.position.add(new Vector2d(620,80));
+  context.translate(rearwheel.position.dx,rearwheel.position.dy);
+  context.rotate(rearwheel.position.dx/(rearwheel.height/2));
+  context.drawImage(rearwheel, -rearwheel.width/2, -rearwheel.height/2);
+  context.restore();
   
-  rearwheel.position.vectorSum(car.position, new Vector2d(672,120));
-  context.drawImage(rearwheel, rearwheel.position.dx, rearwheel.position.dy);
   clampCar();
 }
 
