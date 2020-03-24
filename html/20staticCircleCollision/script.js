6a4f42cddfe03cab02f1df0023c64782d5ae6773
Max Bronstring <max.brons@hotmail.com>
2020-03-24 15:56:40 +0100

    update

--- html/20staticCircleCollision/script.js
+++ html/20staticCircleCollision/script.js
@@ -3,61 +3,49 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let ball = new DPoint(new Vector2d(200,300),new Vector2d(6,6), new Vector2d(0,0),20,"white");
+let bumper = new Point(new Vector2d(canvas.width/2,canvas.height/2),canvas.height/4,"white");
+let distance;
 
-let bumper, player,rad,tan;
-
-function setUp(){
-  bumper = new Point(new Vector2d(canvas.width/2,canvas.height/2),100,"red");
-  player = {};
-  player.pos = new Vector2d(100,100);
-  player.vel = new Vector2d(7,8);
-  player.point = new Point(new Vector2d(player.pos.dx,player.pos.dy),20,"pink");
-
-  rad = new Vector2d(1,1);
-  tan = new Vector2d(1,1);
-  animate();
-}
+ball.rad = new Vector2d(1,1);
+ball.tan = new Vector2d(1,1);
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,canvas.width,canvas.height);
+
+  ball.rad.dx = (bumper.position.dx - ball.pos.dx);
+  ball.rad.dy = (bumper.position.dy - ball.pos.dy);
+
+  if(ball.pos.dx < ball.radius || ball.pos.dx > canvas.width -  ball.radius){
+    ball.vel.dx = -ball.vel.dx;
+  }
+
+  if(ball.pos.dy < ball.radius || ball.pos.dy > canvas.height -  ball.radius){
+    ball.vel.dy = -ball.vel.dy;
+  }
+
+  distance = ball.rad.magnitude;
+  
+  ball.rad.magnitude = 1;
+  ball.rad.magnitude = ball.rad.dot(ball.vel);
+  ball.tan.dx = ball.rad.dy;
+  ball.tan.dy = -ball.rad.dx;
+  ball.tan.magnitude = 1;
+  ball.tan.magnitude = ball.tan.dot(ball.vel);
+
+if(distance < ball.radius + bumper.radius){
+  ball.rad.magnitude = -ball.rad.magnitude;
+  
+  ball.vel.vectorSum(ball.rad,ball.tan);
+}
   bumper.draw(context);
-  player.pos.add(player.vel);
-  player.point.pos(player.pos);
-
-  if(player.pos.dx < player.point.radius || player.pos.dx > canvas.width - player.point.radius){
-    player.vel.dx = -player.vel.dx;
-  }
-  if(player.pos.dy < player.point.radius || player.pos.dy > canvas.height - player.point.radius){
-    player.vel.dy = -player.vel.dy;
-  }
-  rad.magnitude = 3;
-  tan.magnitude = 3;
-  rad.magnitude = player.vel.dot(rad);
-  tan.magnitude = player.vel.dot(tan);
-  rad.draw(context,player.pos.dx,player.pos.dy,10);
-  tan.draw(context,player.pos.dx,player.pos.dy,10);
-
-  rad.dx = bumper.position.dx - player.pos.dx;
-  rad.dy = bumper.position.dy - player.pos.dy;
-
-  tan.dx = -rad.dy;
-  tan.dy = rad.dx;
-
-  if(rad.magnitude < player.point.radius + bumper.radius){
-    rad.magnitude = 1;
-    tan.magnitude = 1;
-    rad.magnitude = player.vel.dot(rad);
-    tan.magnitude = player.vel.dot(tan);
-    rad.draw(context,new Vector2d(player.pos.dx,player.pos.dy),10);
-    tan.draw(context,new Vector2d(player.pos.dx,player.pos.dy),10);
-    rad.angle += Math.PI;
-    player.vel.vectorSum(rad,tan);
-  }
-
-  player.vel.draw(context,new Vector2d(player.pos.dx,player.pos.dy),10);
-
-  player.point.draw(context);
+  bumper.draw(context,ball.vel,10,"red");
+  ball.draw(context);
+  ball.tan.draw(context,ball.pos,15,"yellow");
+  ball.vel.draw(context,new Vector2d(ball.pos.dx + ball.vel.dx,ball.pos.dy + ball.vel.dy),15,"red");
+  ball.rad.draw(context,new Vector2d(ball.pos.dx + ball.vel.dx,ball.pos.dy + ball.vel.dy),15,"yellow");
+  ball.update();
 }
 
-setUp();
+animate();
